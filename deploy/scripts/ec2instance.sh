f24d19f5496f34bec92981f6e96872ba7e523c6c
Breyner Rojas <breyner.rojas@somoglobal.com>
2022-01-09 17:53:21 -0500

    Add script to create Golden AMI

---
+++ deploy/scripts/ec2instance.sh
@@ -0,0 +1,30 @@
+#/bin/sh
+yum update -y
+yum install firewalld -y
+sudo yum install git -y
+
+systemctl start firewalld
+systemctl enable firewalld
+
+## -----------------------------------------------------------------------------------------------------------------------
+# Step 2: Install DOCKER-CE on Machine & Docker-Compose
+
+amazon-linux-extras install docker
+# Add the ec2-user to the docker group so you can execute Docker commands without using sudo.
+usermod -a -G docker ec2-user
+systemctl enable docker && systemctl start docker
+
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Clone repository
+git clone https://Tavet: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@github.com/Tavet/AtomWood.git --branch=master Atomwood
+
+# Enable HTTPS 
+cd Atomwood/nginx
+rm -rf ./default.conf || true
+mv default_https.conf.template default.conf
+
+# Run
+cd ..
+docker-compose up -d
